9ae6f7c42c8519b097abd32447d390696145726b
yulapshun <yulapshun@gmail.com>
2017-03-11 22:26:46 +0800

    Implement pagination

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -4,14 +4,32 @@ namespace app\controllers;
 
 use Yii;
 use yii\rest\ActiveController;
-use app\models\User;
+use yii\data\ActiveDataProvider;
 
 class UserController extends ActiveController
 {
     public $modelClass = 'app\models\User';
 
-    public function checkAccess($action, $model = null, $params = [])
+    public function actions()
     {
-        return true;
+        $actions = parent::actions();
+        $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+        return $actions;
+    }
+
+    public function prepareDataProvider()
+    {
+
+        $modelClass = $this->modelClass;
+
+        return new ActiveDataProvider([
+            'query' => $modelClass::find(),
+            'pagination' => [
+                'defaultPageSize' => 30,
+                'pageSizeLimit' => [0, 100],
+                'pageParam' => 'page',
+                'pageSizeParam' => 'per-page'
+            ]
+        ]);
     }
 }
