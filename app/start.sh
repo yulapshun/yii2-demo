767ecb96d9645553e7754e61a84e82485fd8c076
yulapshun <yulapshun@gmail.com>
2017-03-12 11:42:16 +0800

    Update dependencies and app directory

--- app/start.sh
+++ app/start.sh
@@ -1,4 +1,6 @@
-cd /app
+cd /var/www/app
 composer install --prefer-source --no-interaction --no-progress
+./yii migrate/up --interactive=0
+chmod 777 runtime
 service php7.0-fpm start
 nginx -g 'daemon off;'
